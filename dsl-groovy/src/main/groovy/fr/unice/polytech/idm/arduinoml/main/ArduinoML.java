4af16dc61c2b1976700fbcddad2d0881602f3f5e
LaureenG <laureen.ginier@gmail.com>
2016-01-19 22:40:23 +0100

    commit avant tag0

--- dsl-groovy/src/main/groovy/fr/unice/polytech/idm/arduinoml/main/ArduinoML.java
+++ dsl-groovy/src/main/groovy/fr/unice/polytech/idm/arduinoml/main/ArduinoML.java
@@ -15,11 +15,10 @@ import fr.unice.polytech.idm.arduinoml.dsl.ArduinoMLDSL;
 public class ArduinoML {
 	public static void main(String[] args) {
 		ArduinoMLDSL dsl = new ArduinoMLDSL();
-		// if(args.length > 0) {
-		dsl.eval(new File("./scripts/Switch2.groovy"));
-		// } else {
-		// System.out.println("/!\\ Missing arg: Please specify the path to a
-		// Groovy script file to execute");
-		// }
+		if (args.length > 0) {
+			dsl.eval(new File(args[0])); // "./scripts/Switch2.groovy"
+		} else {
+			System.out.println("/!\\ Missing arg: Please specify the path to a Groovy script file to execute");
+		}
 	}
 }
