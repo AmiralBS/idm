9aa1b726bb00239d8d4ba9eb331340d7c295db0d
Amir BEN SLIMANE <amir.benslimane@gmail.com>
2016-02-16 14:28:04 +0100

    Correction d'un test

--- dsl-groovy/scripts/KonamiCode.groovy
+++ dsl-groovy/scripts/KonamiCode.groovy
@@ -6,6 +6,7 @@ lcd "screen" bus 2 // bus 2 : 10 11 12 13 14 15 16
 
 joystick "joy" on 5, 4, 9 // X, Y, B
 
-konami joy, screen, L, D, A, A, B, R
+konami joy, screen, A, B
+	code L, D, A, A, B, R
 
 export "Konami Code"
