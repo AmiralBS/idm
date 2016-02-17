98743ec37ad8b21591e4d3f2ab02e655209f479a
LaureenG <laureen.ginier@gmail.com>
2016-02-17 17:01:30 +0100

    ajout de scripts scenarii

---
+++ scripts/AnalogicalLed.groovy
@@ -0,0 +1,25 @@
+// 1°) Déclaration des bricks
+input "button" on 8
+output "led" on 3
+
+// 2°) Définition des comportements
+
+// Définition des états
+state "on" means
+	_ led becomes 200
+
+state "off" means
+	_ led becomes low
+
+// Définition des transitions
+from on to off when
+	_ button is high
+
+from off to on when
+	_ button is high
+	
+// 3°) Définition de l'état initial
+initial off
+
+// 4°) Appel pour la génération du code
+export "AnalogicalLed"
